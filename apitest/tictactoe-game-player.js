c378407adcf3bd98432eae4d04d6c9c38fe40f39
Gunnar Örn Baldursson <Gunso_Baldurs@hotmail.com>
2017-12-11 16:53:56 +0000

    Should fail because swapping functions in tictactoe player

--- apitest/tictactoe-game-player.js
+++ apitest/tictactoe-game-player.js
@@ -38,7 +38,7 @@ function playGame(done) {
 
                      Run load tests again. They should fail. Explain why they fail.
          */
-        userB.expectGameJoined().expectMoveMade('X').joinGame(userA.getGame().gameId).then(function () {
+        userB.joinGame(userA.getGame().gameId).expectMoveMade('X').expectGameJoined().then(function () {
             userB.expectMoveMade('O').expectMoveMade('X').placeMove(1, 0).then(() => {
                 userB.expectMoveMade('O').expectMoveMade('X').expectGameWon().placeMove(0, 2).then(() => {
                     userB.disconnect();
@@ -48,7 +48,7 @@ function playGame(done) {
         })
     }
 
-    userA.expectGameCreated().createGame().then(() => {
+    userA.createGame().expectGameCreated().then(() => {
         playOSide();
         userA.expectGameJoined().then(() => {
             userA.expectMoveMade('X').expectMoveMade('O').placeMove(0, 0).then(() => {   //  A
