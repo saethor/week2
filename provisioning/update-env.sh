9b70b00d8fc2cce981d520958ee3bb867a7a8862
Gunnar Örn Baldursson <Gunso_Baldurs@hotmail.com>
2017-12-13 17:28:37 +0000

    Update update-env.sh

--- provisioning/update-env.sh
+++ provisioning/update-env.sh
@@ -37,7 +37,7 @@ ssh -o StrictHostKeyChecking=no -i "~/aws/${SECURITY_GROUP_NAME}.pem" ec2-user@$
 echo Instance ready...going to run this:
 ssh -o StrictHostKeyChecking=no -i "~/aws/${SECURITY_GROUP_NAME}.pem" ec2-user@${INSTANCE_PUBLIC_NAME} "cat ~/docker-compose-and-run.sh"
 
-scp -o StrictHostKeyChecking=no -i "~/aws/${SECURITY_GROUP_NAME}.pem" ./.env ec2-user@${INSTANCE_PUBLIC_NAME}:~/.env
+scp -o StrictHostKeyChecking=no -i "~/aws/${SECURITY_GROUP_NAME}.pem" ../.env ec2-user@${INSTANCE_PUBLIC_NAME}:~/.env
 
 echo Running script
 ssh -o StrictHostKeyChecking=no -i "~/aws/${SECURITY_GROUP_NAME}.pem" ec2-user@${INSTANCE_PUBLIC_NAME} "~/docker-compose-and-run.sh"
